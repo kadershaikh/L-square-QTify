c2f0fb4dfaa97cfa60f14d6e400dababb58ce557
kadershaikh <kadershaikh666@gmail.com>
2024-03-26 20:55:54 +0530

    carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
-import styles from './Carousel.module.css'
+import React from "react";
+import styles from "./Carousel.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation } from "swiper/modules";
+import { useSwiper } from "swiper/react";
+import CarouselLeftNavigation from "./CarouselLeftNavigation/CarouselLeftNavigation";
+import CarouselRightNavigation from "./CarouselRightNavigation/CarouselRightNavigation";
+import { useEffect } from "react";
 
-const Carousel = ({data, renderComponent}) => {
+const Controls = ({ data }) => {
+  const swiper = useSwiper();
+  useEffect(() => {
+    swiper.slideTo(0, 1);
+  }, [data]);
+
+  return <></>;
+};
+
+const Carousel = ({ data, renderComponent }) => {
   return (
-    <div className={styles.carousel}>
-        {data.map((item, index) => (
-        <div key={index} className={styles.cardItem}>
-          {renderComponent(item)}
-        </div>
-      ))}
+    <div className={styles.carouselWrapper}>
+      <Swiper
+        initialSlide={0}
+        modules={{ Navigation }}
+        slidesPerView={7}
+        spaceBetween={10}
+        allowTouchMove
+      >
+        <Controls data={data} />
+        <CarouselLeftNavigation />
+        <CarouselRightNavigation />
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>{renderComponent(item)}</SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
